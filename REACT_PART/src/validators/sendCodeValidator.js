2f32a321c0a3c696c9e6c46fa8b44168423dcd08
Anand Soni <137056068+Anand-Sony@users.noreply.github.com>
2024-09-08 10:28:51 +0530

    Add files via upload

---
+++ REACT_PART/src/validators/sendCodeValidator.js
@@ -0,0 +1,27 @@
+const isEmail = (email) =>
+    String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/  
+            
+            // [^<>()[\]\\.,;:\s@"]+ matches one or more characters that are not special characters
+            // (\.[^<>()[\]\\.,;:\s@"]+)* matches zero or more occurrences of a dot followed by one or more characters that are not special characters
+            // |(".+") matches a quoted string (for email addresses with quotes) 
+        );
+
+const sendCodeValidator = ({email})=>{
+    const errors = {
+        email : ""
+    };
+
+    if (!email) {
+        errors.email = "Email is required"
+        // to check email is valid or not , i have written code on top of the file
+    }
+    else if (!isEmail(email)) {
+        errors.email = "Invalid Email"
+    }
+
+    return errors;
+};
+export default sendCodeValidator;
