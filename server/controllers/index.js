77f57e06c8ca62dbf724b85df8341547566ae27d
Anand Soni <137056068+Anand-Sony@users.noreply.github.com>
2024-12-23 19:12:43 +0530

    index.js

---
+++ server/controllers/index.js
@@ -0,0 +1,4 @@
+const authController = require("./auth");
+const categoryController = require("./category");
+const fileController = require("./file");
+module.exports = {authController , categoryController , fileController};
